e3adf1f8ac90094a23a97ec4f6e8cb79de844fd6
asungy <62207329+asungy@users.noreply.github.com>
2024-09-23 23:16:01 -0400

    I don't know how to return strings...

--- caller/src/main.rs
+++ caller/src/main.rs
@@ -4,17 +4,26 @@ use wasmtime::{
     Module,
     Store,
 };
+use std::os::raw::c_char;
 
 fn main() -> Result<(), anyhow::Error>{
     let engine = Engine::default();
     let module = Module::from_file(&engine, "./target/wasm32-unknown-unknown/debug/client.wasm")?;
     let mut store = Store::new(&engine, ());
     let instance = Instance::new(&mut store, &module, &[])?;
+
+    // my_func
     let my_func = instance.get_func(&mut store, "my_func")
         .expect("`my_func` was not an exported function");
     let my_func = my_func.typed::<(i32, i32), i32>(&store)?;
     let result = my_func.call(&mut store, (2, 7))?;
     println!("Answer: {}", result);
+
+    // other_func
+    let other_func = instance.get_func(&mut store, "other_func")
+        .expect("`other_func` was not an exported function");
+    let other_func = other_func.typed::<(), *mut c_char>(&store)?;
+
 
     Ok(())
 }
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,4 +1,13 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
 pub extern "C" fn my_func(a: i32, b: i32) -> i32 {
     a + b
+}
+
+#[no_mangle]
+pub extern "C" fn other_func() -> *mut c_char {
+    let s = CString::new("hello, world").unwrap();
+    s.into_raw()
 }
